687e946d811ccbb0ad02a3feb88cbb310a87fdf7
Helaree <140392834+Helaree@users.noreply.github.com>
2024-07-04 12:23:14 +0300

    Add files via upload

---
+++ index.php.php
@@ -0,0 +1,22 @@
+<?php
+session_start()
+
+   $_SESSION;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+</head>
+<body>
+
+
+<a href="logout.php">Logout</a>
+<h1>This is my web</h1>
+
+
+<br>
+Hello .Username.
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,51 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Signup</title>
+</head>
+<body>
+
+
+<style type="text/css">
+	#text{
+		height: 25px;
+		border-radius: 5px;
+		padding: 4px;
+		border: solid thin #aaa;
+
+	}
+
+	#button{
+		padding: 10px;
+		width: 100px;
+		color: white;
+		background-color: lightblue;
+		border: none;
+	}
+
+	#box{
+		background-color: grey;
+		margin: auto;
+		width: 300px;
+		padding: 20px;
+	}
+
+</style>
+
+<div id="box">
+	<form method="post">
+		<div style="font-size: 20px;margin: 10px;" ></div>
+		<input type="text" name="User_name"><br><br>
+		<input type="Passord" name="Passord"><br><br>
+
+		<input type="submit" value="Login"><br><br>
+
+		<a href="login.php">Signup</a><br><br>
+	</form>
+	</div>
+</body>
+</html>
